b69497b5fa4efc4728a98e7a74944ba1f78d71e8
ekaragoz86us <144615241+ekaragoz86us@users.noreply.github.com>
2023-09-18 22:04:05 -0500

    Update advanced.py

--- advanced.py
+++ advanced.py
@@ -1,56 +1,94 @@
 # Define hourly rates and tax rates based on levels
-HourlyRate1LevelX = 12  # Replace with the actual hourly rate for regular work for Level X
-HourlyRate2LevelX = 18  # Replace with the actual hourly rate for overtime work for Level X
-
-# Define rates for family support and tax rates by state
-MarriageStatusRate = 50  # Replace with the actual marriage status rate
-ChildRate = 20  # Replace with the actual rate per child
-StateIncomeTaxRates = {
-    "California": 0.1,  # Replace with the actual state tax rate for California
-    # Add more states and their tax rates as needed
+#levels = ["L1", "L2", "L3", "L4", "L5", "L6", "L7", "L8"]
+regular_hourly_rates = {
+    " L1": 10,
+    " L2": 15,
+    " L3": 20,
+    " L4": 30,
+    " L5": 40,
+    " L6": 60,
+    " L7": 80,
+    " L8": 100
 }
-FederalTaxRate = 0.15  # Replace with the actual federal tax rate
+overtime_hourly_rates = {
+    " L1": 15,
+    " L2": 22,
+    " L3": 30,
+    " L4": 45,
+    " L5": 60,
+    " L6": 90,
+    " L7": 120,
+    " L8": 150
+}
+
+# Define rates for family support
+MarriageStatusRate = {
+    " Single": 50,
+    " Married": 150
+}  # Support by marriage status
+ChildRate = 20  # Support rate per child
+
+# Define tax rates
+FederalTaxRate = 0.15  # Federal tax rate
+#states = ["California", "Texas", "Florida", "New York", "Pennsylvania", "Illinois", "Ohio", "Georgia", "North Carolina", "Michigan"]
+StateIncomeTaxRates = {
+    " California": 0.10,
+    " Texas": 0.12,
+    " Florida": 0.07,
+    " New York": 0.15,
+    " Pennsylvania": 0.08,
+    " Illinois": 0.09,
+    " Ohio": 0.03,
+    " Georgia": 0.06,
+    " North Carolina": 0.09,
+    " Michigan": 0.00
+}
 
 # Open the input file for reading
-with open("advance_report.txt", "r") as input_file:
-    # Open the output file for writing
-    with open("advance_salary.txt", "w") as output_file:
-        # Write header to the output file
-        output_file.write("ID, Name, Level, State, Single/Married, #ofChildren, #ofRegularWorkHours, #ofOvertimeHours, GrossIncome, StateTax, FederalTax, NetIncome\n")
+with open("advanced_report.txt", "r") as input_file:
+  # Open the output file for writing
+  with open("advanced_salary.txt", "w") as output_file:
+    # Write header to the output file
+    output_file.write(
+        "ID, Name, Level, State, Single/Married, #ofChildren, #ofRegularWorkHours, #ofOvertimeHours, GrossIncome, StateTax, FederalTax, NetIncome\n"
+    )
 
-        # Iterate over each line in the input file
-        for line in input_file:
-            # Split the line into fields using a comma as the separator
-            fields = line.strip().split(",")
-            
-            # Extract employee information
-            employee_id, employee_name, level, state, marital_status, num_children, reg_work_hour, overtime_hour = fields
-            
-            # Convert work hour values to integers
-            reg_work_hour = int(reg_work_hour)
-            overtime_hour = int(overtime_hour)
-            num_children = int(num_children)
-            
-            # Calculate GrossIncome
-            gross_income = (reg_work_hour * HourlyRate1LevelX) + (overtime_hour * HourlyRate2LevelX)
-            
-            # Calculate FamilySupport
-            family_support = (MarriageStatusRate * (1 if marital_status == "Married" else 0)) + (ChildRate * num_children)
-            
-            # Calculate StateTax
-            state_tax_rate = StateIncomeTaxRates.get(state, 0)  # Get state tax rate or default to 0
-            state_tax = gross_income * state_tax_rate
-            
-            # Calculate FederalTax
-            federal_tax = gross_income * FederalTaxRate
-            
-            # Calculate NetIncome
-            net_income = gross_income - (state_tax + federal_tax)
-            
-            # Format the output line
-            output_line = f"{employee_id}, {employee_name}, {level}, {state}, {marital_status}, {num_children}, {reg_work_hour}, {overtime_hour}, {gross_income}, {state_tax}, {federal_tax}, {net_income}\n"
-            
-            # Write the output line to the output file
-            output_file.write(output_line)
+    # Skip the first line by simply reading it
+    input_file.readline()
+
+    # Iterate over each line in the input file
+    for line in input_file:
+      # Split the line into fields using a comma as the separator
+      fields = line.strip().split(",")
+      print(fields)
+
+      # Extract employee information
+      employee_id, employee_name, level, state, marital_status, num_children, reg_work_hour, overtime_hour = fields
+
+      # Convert work hour values to integers
+      reg_work_hour = int(reg_work_hour)
+      overtime_hour = int(overtime_hour)
+      num_children = int(num_children)
+
+      # Calculate GrossIncome
+      gross_income = (reg_work_hour * regular_hourly_rates[level]) + (
+          overtime_hour * overtime_hourly_rates[level])
+
+      # Calculate FamilySupport
+      family_support = MarriageStatusRate[marital_status] + (ChildRate *
+                                                             num_children)
+
+      # Calculate Tax
+      federal_tax = gross_income * FederalTaxRate
+      state_tax = gross_income * StateIncomeTaxRates[state]
+
+      # Calculate NetIncome
+      net_income = gross_income - (state_tax + federal_tax)
+
+      # Format the output line
+      output_line = f"{employee_id}, {employee_name}, {level}, {state}, {marital_status}, {num_children}, {reg_work_hour}, {overtime_hour}, {gross_income}, {state_tax}, {federal_tax}, {net_income}\n"
+
+      # Write the output line to the output file
+      output_file.write(output_line)
 
 print("Salary calculation and writing to advance_salary.txt is complete.")
